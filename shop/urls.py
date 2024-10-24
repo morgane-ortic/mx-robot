e362df5315e5b19ea02dbc26ce4b0bf39671c6a7
Morgane Ortic <morgane.ortic@proton.me>
2024-10-24 15:57:16 +0200

    implement account functionality with register, login, logout

--- shop/urls.py
+++ shop/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
-from shop.views import add_to_cart, detail, list_categories, sort_category, list_products
+from shop.views import home, login, log_out, register, add_to_cart, detail, list_categories, sort_category, view_cart
 
 app_name = 'shop'
 urlpatterns = [
-    path('', list_products, name='list_products'),
+    path('', home, name='home'),
+    path('login/', login, name='login'),
+    path('log_out/', log_out, name='log_out'),
+    path('register/', register, name='register'),
     path('product/<int:pk>/', detail, name='detail'),
     path('categories/', list_categories, name='categories'),
     path('category/<str:category>/', sort_category, name='category'),
     path('add_to_cart/<int:pk>/', add_to_cart, name='add_to_cart'),  # URL for adding product to cart
-    path()
+    path('view_cart/', view_cart, name='view_cart'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,11 +1,45 @@
 from django.shortcuts import render, redirect, HttpResponseRedirect
-
+import json
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, logout
+from django.contrib.auth.models import User
 from shop.models import Product, Customer, Order, Cart
 
 
-def list_products(request):
+def home(request):
     all_products = Product.objects.all()
-    return render(request, 'shop/index.html', {'products': all_products})
+    user_id = request.session.get('user_id')
+    if user_id is not None:
+        user = User.objects.get(id=user_id)
+        return render(request, 'shop/home.html', {'username': user.username, 'products': all_products})
+    else:
+        return render(request, 'shop/home.html', {'products': all_products})
+
+def login(request):
+    if request.method == 'POST':
+        # print(request.POST.get('password'))
+        user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
+        if user is not None:
+            request.session['user_id'] = user.id
+            return redirect('shop:home')
+        else:
+            return redirect('shop:login')
+    return render(request, 'shop/login.html')
+
+def log_out(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('shop:home')
+    return redirect('shop:home')
+    
+def register(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+        User.objects.create_user(username=username, password=password, email=email)
+        return redirect('shop:login')
+    return render(request, 'shop/register.html')
 
 
 def detail(request, pk):
@@ -23,22 +57,4 @@ def sort_category(request, category):
     category_products = Product.objects.filter(category=category)
     if not category_products.exists():
         return redirect('/')
-    return render(request, 'shop/index.html', {'products': category_products})
-
-def add_to_cart(request, pk):
-    product = Product.objects.get(id=pk)
-
-    if 'temp_customer_id' not in request.session:
-        temp_customer = Customer.objects.create(first_name='Temp', last_name='Customer')
-        request.session['temp_customer_id'] = temp_customer.id
-    else:
-        temp_customer = Customer.objects.get(id=request.session['temp_customer_id'])
-    customer = temp_customer
-
-    # Add the product to the cart
-    cart_item, created = Cart.objects.get_or_create(customer=customer, product=product)
-    if not created:
-        cart_item.quantity += 1
-        cart_item.save()
-
-    return redirect('shop:list_products')
+    return render(request, 'shop/home.html', {'products': category_products})
